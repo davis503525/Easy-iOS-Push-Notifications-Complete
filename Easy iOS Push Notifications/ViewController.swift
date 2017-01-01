ee4a379147bdf84c740901ee461c5cf2876003ed
Davis Allie <davisallie@daviss-mbp.gateway>
2017-01-01 18:10:57 +1100

    Initial Commit

---
+++ Easy iOS Push Notifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Easy iOS Push Notifications
+//
+//  Created by Davis Allie on 1/1/17.
+//  Copyright © 2017 Davis Allie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Easy iOS Push NotificationsTests/Easy_iOS_Push_NotificationsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Easy_iOS_Push_NotificationsTests.swift
+//  Easy iOS Push NotificationsTests
+//
+//  Created by Davis Allie on 1/1/17.
+//  Copyright © 2017 Davis Allie. All rights reserved.
+//
+
+import XCTest
+@testable import Easy_iOS_Push_Notifications
+
+class Easy_iOS_Push_NotificationsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
